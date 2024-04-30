194a04233e81ad77d5c2e4cab504453e93f8adb1
Nour Eldeen <noureldeenh5@gmail.com>
2024-04-30 20:17:53 +0300

    exceptions task

---
+++ src/main/java/com/example/exceptions/T1/Main.java
@@ -0,0 +1,42 @@
+package com.example.exceptions.T1;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        boolean inputValid = false;
+        int num1 = 0, num2 = 0;
+
+        while (!inputValid) {
+            try {
+                System.out.print("Enter the first number: ");
+                num1 = input.nextInt();
+                inputValid = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Your inpur should be in integer type.");
+                input.next();
+            }
+        }
+
+        inputValid = false;
+
+        while (!inputValid) {
+            try {
+                System.out.print("Enter the second number: ");
+                num2 = input.nextInt();
+                inputValid = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Your inpur should be in integer type.");
+                input.next();
+            }
+        }
+
+        int sum = ArithmeticOperations.sum(num1, num2);
+        System.out.println(sum);
+        input.close();
+
+    }
+}
